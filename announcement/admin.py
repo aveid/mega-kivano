8c8b0aa9207685401f698cf3e7ac8ca559c0bcda
aveid <tabyldieva1296@gmail.com>
2025-04-25 08:42:16 +0600

    add endpoints

--- announcement/admin.py
+++ announcement/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from announcement.models import Item, Recall
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', "price", "created_at", "updated_at",
+                    "count", "sub_category", "article"]
+
+
+admin.site.register(Item, ItemAdmin)
+
+
+class RecallAdmin(admin.ModelAdmin):
+    list_display = ['id', 'full_name', 'rate']
+
+
+admin.site.register(Recall, RecallAdmin)
--- category/views.py
+++ category/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.generics import (ListAPIView,
+                                     RetrieveAPIView,
+                                     CreateAPIView)
 
-# Create your views here.
+from category.models import Category
+from category.serializers import CategorySerializer
+
+
+class CategoryListAPIView(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetailAPIView(RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryCreateAPIView(CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
